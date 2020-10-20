4b9f5b8a3f461789b5e73ca34186fdd268865a9f
butylacetate <butyl.acetate@gmail.com>
2020-10-20 14:18:58 +0400

    Fix sequences

--- src/main/java/com/haulmont/testtask/domain/Recipe.java
+++ src/main/java/com/haulmont/testtask/domain/Recipe.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -18,7 +19,7 @@ import javax.persistence.Table;
 public class Recipe implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String description;
